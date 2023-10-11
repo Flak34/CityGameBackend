bbee817c1087f26b0ca9e67f7bc2be85e5b6d0de
Andrey Perfilyev <a.perfilyev03@gmail.com>
2023-10-11 19:43:04 +0300

    Added tests for tdd of class CitiesService

---
+++ src/test/java/com/example/citygamebackend/CitiesServiceTest.java
@@ -0,0 +1,33 @@
+package com.example.citygamebackend;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+public class CitiesServiceTest {
+    @Test
+    @DisplayName("Check is city exist or not")
+    public void checkIsCityExistsSuccessCase1(){
+        CitiesService citiesService = new CitiesService("20K+cities.txt");
+        assertTrue(citiesService.isCityExist("Москва"));
+    }
+
+    @Test
+    @DisplayName("Check is city exist or not")
+    public void checkIsCityExistsSuccessCase2(){
+        CitiesService citiesService = new CitiesService("20K+cities.txt");
+        assertTrue(citiesService.isCityExist("марафон"));
+    }
+
+    @Test
+    @DisplayName("Check is city exists or not")
+    public void checkIsCityExistNotSuccess(){
+        CitiesService citiesService = new CitiesService("20K+cities.txt");
+        assertFalse(citiesService.isCityExist("Найт сити"));
+    }
+}
